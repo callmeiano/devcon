1f58ec52a251551deafd96afdcc7d274b392f2af
callmeiano <strongmoises2002@gmail.com>
2024-07-18 15:34:01 +0800

    change page content

--- devcon-moises/app/page.tsx
+++ devcon-moises/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Hello, my name is DevCon-Strong.
+      A CCS 4001 Seminar in collaboration with DEVCON PH: Iloilo Chapter.
     </main>
   );
 }
